663bea23c74cf062a6ee76cbb04887681321c08c
MTruong2000 <truongdev2000@gmail.com>
2025-02-10 09:59:02 +0700

    add file env in project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const db = require('./firebase-config'); // Import Firestore config
 const cors = require("cors");
+const db = require('./firebase-config'); // Import Firestore config
 
 const app = express();
 
