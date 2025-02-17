856ce9b9b0532f49f7b03993cfee7a2f6c2bdd6f
MTruong2000 <truongdev2000@gmail.com>
2025-02-17 10:34:17 +0700

    Remove library not use

--- index.js
+++ index.js
@@ -33,25 +33,6 @@ app.use(cors({
 // Middleware để parse body request thành JSON
 app.use(express.json());
 
-// API để lưu dữ liệu vào Firestore
-// app.post('/api/save-firestore-not-complete', async (req, res) => {
-//     try {
-//         const { userId, customerData } = req.body;
-
-//         if (!userId || !customerData) {
-//             return res.status(400).json({ message: 'Missing userId or customerData' });
-//         }
-
-//         // Lưu dữ liệu vào Firestore
-//         await db.collection('customer-answer-not-complete').doc(userId).set(customerData);
-
-//         return res.status(200).json({ message: 'Data saved successfully' });
-//     } catch (error) {
-//         console.error('Error saving data to Firestore:', error);
-//         return res.status(500).json({ message: 'Error saving data to Firestore' });
-//     }
-// });
-
 async function fetchLarkToken() {
     try {
         const response = await axios.post(process.env.LARK_URL_GET_TOKEN, {
